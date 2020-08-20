05c6c71972e08381466cb8121d61bdd9441e06d0
Joey Bortfeld <jbortfeld@yahoo.com>
2020-08-19 17:37:55 -0700

    remove unused

--- config.py
+++ config.py
@@ -6,30 +6,6 @@ if borders:
 else:
     border = '0px solid red'
 
-# slate grey: 3f4d5e
-
-# colors = {
-#
-#
-#
-#
-#     'background': '#0C1B33',
-#     'plot_background': '#0C1B33',
-#     'paper_background': '#0C1B33',
-#     'line': '#03B5AA',
-#     'line2': '#FF6978',
-#     'line3': '#A42CD6',
-#     'font': '#03B5AA',
-#     'axis': 'white',
-#     'title': '#0C1B33',
-#     'zero_vertical': 'red',
-#     'intro_text': '#79C99E',
-#     'table_font': '#79C99E',
-#     'discussion_text': '#79C99E',
-#     'discussion_text_2': 'green'
-# }
-
-
 
 colors = {
 
